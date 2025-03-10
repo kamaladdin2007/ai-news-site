7be8a4640851001ec0f5dbbb065019d491c7b8e3
Kamaladdin07 <127054759+kamaladdin2007@users.noreply.github.com>
2025-03-10 18:10:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("AI-сайт запущен!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Добро пожаловать на AI-сайт!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
